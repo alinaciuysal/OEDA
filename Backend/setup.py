68d2babf50a39e2193f8e0c70ee35b22398d6ca6
alinaciuysal <alinaciuysal@gmail.com>
2018-07-20 14:59:14 +0200

    added licenses for packages in backend & frontend

--- Backend/setup.py
+++ Backend/setup.py
@@ -9,26 +9,25 @@ setup(
     include_package_data=False,
     zip_safe=False,
     install_requires=[
-        'Tempita',
-        'colorama', # color on the console
-        'tornado',
-        'flask_restful',
-        'flask_cors',
-        'elasticsearch>=6.0.0,<7.0.0', # elasticsearch integration
-        'statsmodels', # statistics and statistical testing
-        'numpy', # scientific computing
-        'requests', # http integreation
-        'freetype-py',
-        'pypng',
-        'matplotlib',
-        'pyjwt',
-        'backports.ssl_match_hostname',
-        'scikit-optimize', # gauss optimizer
-        'kafka', # integration with kafka
-        'paho-mqtt',
-        'pandas',
-        'seaborn',  # plotting lib
-        'paho-mqtt',  # mqtt integration
-        'scipy' # numerics and statistics
+        'Tempita', # MIT license
+        'colorama', # BSD license (BSD)
+        'tornado', # Apache license
+        'flask_restful', # BSD License (BSD)
+        'flask_cors', # MIT License (MIT)
+        'elasticsearch>=6.0.0,<7.0.0', # Apache Software License (Apache License, Version 2.0)
+        'statsmodels', # statistics and statistical testing, BSD License (BSD License)
+        'numpy>=1.14.2', # scientific computing, OSI Approved (BSD)
+        'requests', # http integreation, Apache Software License (Apache 2.0)
+        'freetype-py', # bindings for the FreeType library, GNU General Public License (GPL)
+        'pypng', # PNG image files to be read and written using pure Python, MIT License
+        'matplotlib', # Python Software Foundation License (BSD)
+        'pyjwt', # JSON Web Token implementation in Python, MIT License (MIT)
+        'backports.ssl_match_hostname', # The ssl.match_hostname() function from Python 3.5, Python Software Foundation License
+        'scikit-optimize>=0.5.2', # gauss optimizer, BSD
+        'kafka', # Pure Python client for Apache Kafka, Apache Software License (Apache License 2.0)
+        'paho-mqtt', # MQTT version 3.1.1 client class, OSI Approved (Eclipse Public License v1.0 / Eclipse Distribution License v1.0)
+        'pandas', # Powerful data structures for data analysis, time series, and statistics, BSD
+        'seaborn', # statistical data visualization, BSD License (BSD (3-clause))
+        'scipy' # Scientific Library for Python, BSD License (BSD)
     ]
 )
